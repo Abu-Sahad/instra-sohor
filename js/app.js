98b8a2a5359591ea2e59c8a0faa37488cb6bc24f
Abu-Sahad <abusahad603@gmail.com>
2022-03-11 00:29:41 +0600

    heart and report multi value

--- js/app.js
+++ js/app.js
@@ -39,15 +39,18 @@ const displayContent = (text) => {
 };
 
 const switchTab = (id) => {
+  document.getElementById("liked").innerHTML = '';
+  document.getElementById("reported").innerHTML = '';
   if (id === "posts") {
+
     document.getElementById("posts").style.display = "grid";
     document.getElementById("liked").style.display = "none";
     document.getElementById("reported").style.display = "none";
   } else if (id === "liked") {
+
     document.getElementById("liked").style.display = "block";
     document.getElementById("posts").style.display = "none";
     document.getElementById("reported").style.display = "none";
-
     displayLikedPosts();
   } else {
     document.getElementById("reported").style.display = "block";
@@ -159,7 +162,7 @@ const displayLikedPosts = () => {
 
 const displayReportedPosts = () => {
   const reportedPosts = getReportedPosts();
-  posts.forEach((post) => {
+  reportedPosts.forEach((post) => {
     const div = createPost(post);
     document.getElementById("reported").appendChild(div);
   });
